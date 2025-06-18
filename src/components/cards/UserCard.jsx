bf86e367c798efb6c815a13ccba0bb7ba83a834d
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-06-17 21:59:14 -0600

    userCard updeted

--- src/components/cards/UserCard.jsx
+++ src/components/cards/UserCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import UserStatusInfoCard from "./UserStatusInfoCard";
+// import UserStatusInfoCard from "./UserStatusInfoCard";
 const UserCard = ({ userInfo }) => {
   return (
     <div className="user-card p-2 cursor-pointer">
@@ -40,3 +40,26 @@ const UserCard = ({ userInfo }) => {
 };
 
 export default UserCard;
+
+const UserStatusInfoCard = ({ label, count, status }) => {
+  const handleStatusColor = () => {
+    switch (status) {
+      case "Progress":
+        return "text-yellow-500 bg-gray-50";
+
+      case "Complete":
+        return "text-green-700 bg-gray-50";
+
+      default:
+        return "text-orange-500 bg-gray-50";
+    }
+  };
+
+  return (
+    <div
+      className={`flex-1 items-center text-[12px] font-medium ${handleStatusColor()} px-2 py-0.5 rounded `}
+    >
+      <span className="text-[14px] font-semibold">{count}</span> <br /> {label}
+    </div>
+  );
+};
